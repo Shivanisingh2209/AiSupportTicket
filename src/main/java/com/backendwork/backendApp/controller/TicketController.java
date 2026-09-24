7d4304d0a82ad9077fa0ad26e3c4931b061c3cf7
Shivanisingh2209 <shivanisinghvnsbhu@gmail.com>
2026-09-24 18:24:49 +0530

    DTO and Validation

--- src/main/java/com/backendwork/backendApp/controller/TicketController.java
+++ src/main/java/com/backendwork/backendApp/controller/TicketController.java
@@ -1,7 +1,10 @@
 package com.backendwork.backendApp.controller;
 
+import com.backendwork.backendApp.dto.CreateTicketRequest;
+import com.backendwork.backendApp.dto.UpdateTicketRequest;
 import com.backendwork.backendApp.entity.Ticket;
 import com.backendwork.backendApp.services.TicketService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +20,15 @@ public class TicketController {
     private TicketService ticketService;
 
     @PostMapping
-    public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket) {
+    public ResponseEntity<Ticket> createTicket(@Valid @RequestBody CreateTicketRequest request) {
+        Ticket ticket = new Ticket();
+
+        ticket.setCustomerName(request.getCustomerName());
+        ticket.setCustomerEmail(request.getCustomerEmail());
+        ticket.setDescription(request.getDescription());
+        ticket.setPriority(request.getPriority());
+        ticket.setSubject(request.getSubject());
+
         Ticket createdTicket = ticketService.createTicket(ticket);
 
         return ResponseEntity
@@ -54,8 +65,17 @@ public class TicketController {
     @PutMapping("/{id}")
     public ResponseEntity<Ticket> updateTicket(
             @PathVariable String id,
-            @RequestBody Ticket ticket
+            @RequestBody UpdateTicketRequest request
     ) {
+        Ticket ticket = new Ticket();
+
+        ticket.setCustomerName(request.getCustomerName());
+        ticket.setCustomerEmail(request.getCustomerEmail());
+        ticket.setStatus(request.getStatus());
+        ticket.setSubject(request.getSubject());
+        ticket.setPriority(request.getPriority());
+        ticket.setDescription(request.getDescription());
+
         Ticket updatedTicket = ticketService.updateTicket(id, ticket);
 
         if (updatedTicket == null) {
